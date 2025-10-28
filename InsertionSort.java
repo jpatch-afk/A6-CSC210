9868c21885327ec0c7c66a9f949fc46a1da0b83e
jpatch-afk <jpaczkowski@smith.edu>
2025-10-28 19:39:45 -0400

    Adding implementation of sorting

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,6 @@
+import java.util.ListIterator;
+import java.util.Collections;
+
 public class InsertionSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
@@ -5,18 +8,75 @@ public class InsertionSort {
     // register the starting configuration with the recorder
     record.add(unsorted);
 
-    // Here is the result list you will be creating
     CardPile sorted = new CardPile();
-  
-    // ***********************************************************
-    // Here is where you'll do the "work" of InsertionSort:
-    //   - Use sorted to store the "sorted portion"
-    //   - Don't forget to register the new state with the
-    //     recorder after each card is transferred:
 
-    // ***********************************************************
+    Card card = unsorted.getFirst();
 
-    // return the sorted result here
+    sorted.add(card);
+    sorted.remove(card);
+
+
+    record.next();
+    record.add(sorted);   
+    record.add(unsorted); 
+
+    ListIterator<Card> pos = sorted.listIterator();
+   
+    while(!unsorted.isEmpty()){
+      Card new_Card = unsorted.getFirst();
+
+      int compare = new_Card.compareTo(sorted.getFirst());
+
+      if(compare == 0){
+        sorted.insertAfter(new_Card, sorted.getFirst());
+      }
+      else if(compare < 0){
+        sorted.insertBefore(new_Card, sorted.getFirst());
+      }
+      else if(compare > 0) {
+        Card sorted_card = pos.next();
+        while(compare > 0){
+          compare = new_Card.compareTo(sorted_card);
+
+          if(compare == 0) {
+            sorted.insertAfter(new_Card, sorted_card);
+          }
+          else if(compare < 0){
+            sorted.insertBefore(new_Card, sorted_card);
+          }
+          else if(compare > 0){
+            pos.next();
+          }
+        }
+      }
+
+      record.next();
+      record.add(sorted);   
+      record.add(unsorted); 
+    }
+
+    
+
+    
     return sorted;
+  }
+
+  public static void main(String[] args) {
+    
+    SortRecorder recorder = new SortRecorder();
+
+    // set up the deck of cards
+    Card.loadImages(recorder);
+    
+    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+    Collections.shuffle(cards);
+
+    cards = InsertionSort.sort(cards, recorder);
+
+    System.out.println(cards);
+
+    recorder.display("Card Sort Demo: InsertionSort");
+
   }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,6 @@
+import java.util.ListIterator;
+import java.util.Collections;
+
 public class SelectionSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
@@ -5,20 +8,57 @@ public class SelectionSort {
     // register the starting configuration with the recorder
     record.add(unsorted);
 
-    // Here is the result list you will be creating
+    //new sorted pile
     CardPile sorted = new CardPile();
-  
-    // ***********************************************************
-    // Here is where you'll do the "work" of SelectionSort:
-    //   - Use sorted to store the "sorted portion"
-    //   - Don't forget to register the new state with the
-    //     recorder after each card is transferred:
-    //        record.next();        // tell it this is a new step
-    //        record.add(sorted);   // the sorted pile
-    //        record.add(unsorted); // the unsorted pile
-    // ***********************************************************
 
+    //Iterator for the unsorted pile
+    ListIterator<Card> pos = unsorted.listIterator();
+
+
+    while(!unsorted.isEmpty()){
+      Card min = unsorted.getFirst();
+      Card new_card = pos.next();
+      while(pos.hasNext()) {
+        int compare = min.compareTo(new_card); 
+
+        if(compare == 0) {
+          min = new_card;
+          pos.next();
+        }
+        else if(compare < 0) {
+          pos.next();
+        }
+        else if(compare >= 0){
+          min = new_card;
+          pos.next();
+        }
+      }
+      unsorted.remove(min);
+      sorted.add(min);
+
+      record.next();
+      record.add(sorted);   
+      record.add(unsorted); 
+    }
     // return the sorted result here
     return sorted;
+  }
+
+public static void main(String[] args) {
+  SortRecorder recorder = new SortRecorder();
+
+  // set up the deck of cards
+  Card.loadImages(recorder);
+  
+  CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+  //mixing up cards
+  Collections.shuffle(cards);
+
+  cards = SelectionSort.sort(cards, recorder);
+
+  System.out.println(cards);
+
+  recorder.display("Card Sort Demo: SelectionSort");
   }
 }
